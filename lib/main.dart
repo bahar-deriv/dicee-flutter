57e55761894b94beaa1091c8ff6aab3087b925bf
Bahar <bahar@regentmarkets.com>
2023-05-11 14:07:28 +0800

    textButton added around the image

--- lib/main.dart
+++ lib/main.dart
@@ -22,14 +22,14 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {},
               child: Image.asset("images/dice1.png"),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {},
               child: Image.asset("images/dice1.png"),
             ),
           ),
